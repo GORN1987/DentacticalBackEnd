489b7644944cb0856bdfb189056a2000ab38dbae
Guilherme Pena Marinho <guilhermepmarinho@gmail.com>
2021-07-29 23:40:17 +0100

    implementing mysql

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,8 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = '0';
   await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
